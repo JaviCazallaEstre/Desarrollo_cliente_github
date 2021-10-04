03353f6a2ce29c85ee6eab280a70c8ddf41c2644
Javier Cazalla Estrella <jcazest2202@g.educaand.es>
2021-10-04 14:13:14 +0200

    Trabajo diario calse 4-10-2021

--- Practicas/Fechas/Ejercicio1.js
+++ Practicas/Fechas/Ejercicio1.js
@@ -2,6 +2,51 @@
 1- Campo de texto donde ponemos una fecha y la validamos
 2- Campo de texto donde ponemos una fecha y la sacamos los años que tienes
 3- Campo de texto donde ponemos una fecha y nos dice los dias que llevamos vivos y las horas
-4- Metemos 2 fechas y calculamos el tiempo entre fechas
+4- Metemos 2 fechas y calculamos el tiempo entre fechas. Dias meses y años
 5- Fecha nacimiento y decimos cuanto falta hasta la jubilacion (65 años)
  */
+
+//Funcion que nos valida la fecha
+function toFecha(fechaString) {
+  var fecha = 0;
+  var componentesFecha = fechaString.split("/");
+  if (componentesFecha.length == 3) {
+    fecha = new Date(
+      componentesFecha[2],
+      componentesFecha[1] - 1,
+      componentesFecha[0]
+    );
+  }
+  return fecha;
+}
+function validaFecha(fechaString) {
+  var fecha = toFecha(fechaString);
+  var componentesFecha = fechaString.split("/");
+  if (componentesFecha.length == 3) {
+    if (
+      fecha.getFullYear == componentesFecha[2] &&
+      fecha.getMonth == componentesFecha[1] - 1 &&
+      fecha.getDate == componentesFecha[0]
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+function anyosFecha(fechaString) {
+  var fecha = toFecha(fechaString);
+  var hoy = new Date();
+  var edad = hoy.getFullYear() - fecha.getFullYear();
+  var mes = hoy.getMonth() - fecha.getMonth();
+  if (mes < 0 || (mes === 0 && hoy.getDate() < edad.getDate())) {
+    edad--;
+  }
+  return edad;
+}
+function restaFechas(fechaString1, fechaString2) {
+  var fecha1 = toFecha(fechaString1);
+  var fecha2 = toFecha(fechaString2);
+}
