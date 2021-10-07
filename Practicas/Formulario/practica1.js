35b73b6626020e5e6064c2b6bafea3f7c3e80111
Javier Cazalla Estrella <jcazest2202@g.educaand.es>
2021-10-07 13:26:43 +0200

    Trabajo clase dia 7 de octubre 2021

---
+++ Practicas/Formulario/practica1.js
@@ -0,0 +1,20 @@
+/* Formulario de aseguradora de coches pedimos tambien ponemos empresa
+-DNI
+-Nombre
+-Apellidos
+-Direccion
+-CP
+-Provincia readOnly
+-Municipio readOnly
+-Fecha nacimiento
+-Fecha carnet
+-telefono
+-Matricula
+-Marca
+-Fecha matriculacion
+-Modelo
+-Cilindrada
+-Puertas(entre 2 y 5)
+-Extras(opcional)
+validar y entregar en json
+*/
